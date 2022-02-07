61fdb0bbf68de6cf6cc37753b61228f3071c328e
ubatyr <ubatyr@mail.ru>
2022-02-07 12:53:35 +0500

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args){
+        System.out.println("Batyr");
+        System.out.println("05.07.1980");
+    }
+}
