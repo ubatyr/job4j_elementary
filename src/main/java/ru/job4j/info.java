8dccc741bcc863fec077180a8c700dbc890eb871
ubatyr <ubatyr@mail.ru>
2022-02-07 13:35:28 +0500

    6. Checkstyle [#4123]

--- src/main/java/ru/job4j/info.java
+++ src/main/java/ru/job4j/info.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
-public class info {
-    public static void main(String[] arge) {
+public class Info {
+    public static void main(String[] args) {
         System.out.println("07.02.2022");
     }
 }
