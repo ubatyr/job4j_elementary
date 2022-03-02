7ccc0fa96cc76918182f3bfcde811034f67a9e9c
ubatyr <ubatyr@mail.ru>
2022-03-02 10:08:35 +0500

    6.6.3. Двухмерный массив. If. [#306727]

---
+++ src/test/java/ru/job4j/array/SkipNagativeTest.java
@@ -0,0 +1,39 @@
+package ru.job4j.array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SkipNagativeTest {
+
+    @Test
+    public void whenArrayRowAndColumnEquals() {
+        int[][] array = {
+                {1, -2},
+                {1, 2}
+        };
+        int[][] result = SkipNagative.skip(array);
+        int[][] expected = {
+                {1, 0},
+                {1, 2}
+        };
+        Assert.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void whenArrayRow4AndColumnNotEquals() {
+        int[][] array = {
+                {1, -2},
+                {1, 2, -3},
+                {1, -2, 3, -4},
+                {1, 2, -3, -4, 5}
+        };
+        int[][] result = SkipNagative.skip(array);
+        int[][] expected = {
+                {1, 0},
+                {1, 2, 0},
+                {1, 0, 3, 0},
+                {1, 2, 0, 0, 5}
+        };
+        Assert.assertArrayEquals(expected, result);
+    }
+}
