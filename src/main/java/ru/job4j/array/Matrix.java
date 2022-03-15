7dc067f59b41236f3c6b2f1bf75e97e120231438
ubatyr <ubatyr@mail.ru>
2022-03-15 11:39:13 +0500

    1. Чтение кода и базовые ошибки [#40205]

--- src/main/java/ru/job4j/array/Matrix.java
+++ src/main/java/ru/job4j/array/Matrix.java
@@ -3,9 +3,9 @@ package ru.job4j.array;
 public class Matrix {
     public static int[][] multiple(int size) {
         int[][] array = new int[size][size];
-        for (int i = 0; i < size; i++) {
+        for (int index = 0; index < size; index++) {
             for (int j = 0; j < size; j++) {
-                array[i][j] = (i + 1) * (j + 1);
+                array[index][j] = (index + 1) * (j + 1);
             }
         }
         return array;
